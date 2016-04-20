110f009eb75fcd0586da04176a4a877f01d9ff40
Cristian Guerrero <cristian@s9.cl>
2016-04-20 11:15:21 -0700

    Add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
-resources :campaigns
+  resources :users, only: [:new, :create]
+  resources :campaigns
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'campaigns#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "validations" do
+    it "requires a first_name" do
+      u = User.new FactoryGirl.attributes_for(:user).merge({first_name: nil})
+      expect(u).to be_invalid
+    end
+
+    it "requires a last_name" do
+      u = User.new FactoryGirl.attributes_for(:user).merge({last_name: nil})
+      expect(u).to be_invalid
+    end
+
+    it "requires an email" do
+      u = User.new FactoryGirl.attributes_for(:user).merge({email: nil})
+      expect(u).to be_invalid
+    end
+
+    it "requires a unique email" do
+      u  = FactoryGirl.create(:user)
+      u2 = User.new FactoryGirl.attributes_for(:user).merge({email: u.email})
+      expect(u2).to be_invalid
+    end
+  end
+
+  describe ".full_name" do
+    it "returns a concatenated first and last name" do
+    u = User.new FactoryGirl.attributes_for(:user).merge({first_name: "John", last_name: "Smith"})
+    expect(u.full_name).to eq("John Smith")
+    end
+  end
+
+  describe "hashing the password" do
+    it "generates a password digest" do
+    u = User.new FactoryGirl.attributes_for(:user)
+    u.save
+    expect(u.password_digest).to be
+    end
+  end
+end
